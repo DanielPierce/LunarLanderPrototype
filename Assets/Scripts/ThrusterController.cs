62e2d5dd2ea0acce18648cb2b4681c0b7edf8a99
Danny Pierce <danny@pierceplace.net>
2021-02-23 19:40:08 -0600

    Basic lander physics
    Lander goes up, lander goes down

---
+++ Assets/Scripts/ThrusterController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrusterController : MonoBehaviour
+{
+
+    public Rigidbody lander;
+    [Header("Thrust in newtons")]
+    public int thrust;
+    [Header("Experienced gravity in m/s/s")]
+    public float gravity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lander.drag = 0;
+    }
+
+    void FixedUpdate()
+    {
+        lander.AddForce(Vector3.down * gravity, ForceMode.Acceleration);
+
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            lander.AddForce(Vector3.up * thrust * Time.deltaTime, ForceMode.Impulse);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
